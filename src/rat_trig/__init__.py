e2858f5ff94a385b7f633006f1b320995d64bc48
Wai-Shing Luk <luk036@gmail.com>
2026-07-09 09:25:02 +0800

    build: exclude skeleton.py from coverage; add pragma (53%→100%)

--- src/rat_trig/__init__.py
+++ src/rat_trig/__init__.py
@@ -42,7 +42,7 @@ if sys.version_info[:2] >= (3, 8):
     from importlib.metadata import version
 else:
     from importlib_metadata import PackageNotFoundError  # pragma: no cover
-    from importlib_metadata import version
+    from importlib_metadata import version  # pragma: no cover
 
 try:
     # Change here if project is renamed and does not equal the package name
